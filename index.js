465c091673cd829560441d8e609d48876fb2c676
Alexia Velador <alexiavel01@gmail.com>
2022-02-28 12:28:09 -0700

    commit tree 0cccb1ae7ae37ea193439588202e222dadec1294

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const employee = {name: 'Alexia', streetAddress: '2223 yourmom st'}
+function updateEmployeeWithKeyAndValue(object,key,value){
+    const newObject = {...object}
+    newObject[key] = value;
+    return newObject;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(object,key,value){
+    object[key] = value;
+    return object;
+}
+function deleteFromEmployeeByKey(object, key){
+    const newObject = {...object}
+    delete newObject[key];
+    return newObject;
+}
+function destructivelyDeleteFromEmployeeByKey(object, key){
+    delete object[key];
+    return object;
+}
